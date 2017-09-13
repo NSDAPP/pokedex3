6d409d923593ffe32d74896341668012d408576a
Nikolai Dirks <NSD@nikolais-macbook-pro.local>
2017-09-13 10:37:43 +0200

    Initial Commit

---
+++ PokeDex3Tests/PokeDex3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PokeDex3Tests.swift
+//  PokeDex3Tests
+//
+//  Created by Nikolai Dirks on 13/09/2017.
+//  Copyright © 2017 NSD. All rights reserved.
+//
+
+import XCTest
+@testable import PokeDex3
+
+class PokeDex3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
